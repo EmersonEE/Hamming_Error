3701f598858427feb257c7db07e960c17d3cc3cc
EmersonEE <perezemerson85@gmail.com>
2024-06-12 09:05:40 -0600

    Proyecto Terminado

---
+++ Hamming_Error.py
@@ -0,0 +1,248 @@
+import pandas as pd
+
+
+def obtener_palabra_valida():
+    while True:
+        palabra = input("Ingrese la palabra en 0 y 1: ")
+        if len(palabra) > 4:
+            print("Palabra Mayor a 4 Bits")
+            print("Por favor, ingrese nuevamente.")
+        elif all(caracter == "0" or caracter == "1" for caracter in palabra):
+            return palabra
+        else:
+            print("Palabra no válida.")
+            print("Por favor, ingrese nuevamente.")
+
+
+numeros_binarios = ["000", "001", "010", "011", "100", "101", "110", "111"]
+
+# Solicita una palabra válida
+palabra = obtener_palabra_valida()
+sumap1 = 0
+sumap2 = 0
+sumap3 = 0
+v_paridad1 = 0
+v_paridad2 = 0
+v_paridad3 = 0
+bitsuma = 0
+sumap1e = 0
+sumap2e = 0
+sumap3e = 0
+v_paridad1e = 0
+v_paridad2e = 0
+v_paridad3e = 0
+# Convierte la palabra en una lista de caracteres
+lista = list(palabra)
+bitM = lista[3]
+# lista[0] = 0 A D1
+# lista[1] = 1 B D2
+# lista[2] = 0 C D3
+# lista[3] = 1 D D4
+# A B C D
+# 0 1 0 1
+print(f"Bit 0: {bitM}")
+
+print(f"El Bit Seleccionado es: {''.join(lista)}")
+
+
+# Paridad 1
+
+if int(lista[0]) == 1:
+    sumap1 += 1
+    sumap2 += 1
+else:
+    sumap1 = sumap1
+    sumap2 = sumap2
+
+if int(lista[1]) == 1:
+    sumap1 += 1
+    sumap3 += 1
+else:
+    sumap1 = sumap1
+    sumap3 = sumap3
+
+if int(lista[3]) == 1:
+    sumap1 += 1
+    sumap2 += 1
+    sumap3 += 1
+else:
+    sumap1 = sumap1
+    sumap2 = sumap2
+    sumap3 = sumap3
+
+if sumap1 % 2:
+    v_paridad1 = 1
+else:
+    v_paridad1 = 0
+
+# Parida 2
+# lista0,lista2,lista3
+if int(lista[2]) == 1:
+    sumap2 += 1
+    sumap3 += 1
+else:
+    sumap2 = sumap2
+    sumap3 = sumap3
+
+if sumap2 % 2:
+    v_paridad2 = 1
+else:
+    v_paridad2 = 0
+
+# Paridad 3
+# lista1,lista2,lista3
+if sumap3 % 2:
+    v_paridad3 = 1
+else:
+    v_paridad3 = 0
+
+lista_info = [
+    str(v_paridad1),
+    str(v_paridad2),
+    lista[0],
+    str(v_paridad3),
+    lista[1],
+    lista[2],
+    lista[3],
+]
+print(f"Codigo de Hamming: {''.join(lista_info)}")
+
+
+def obtener_error_valida():
+    while True:
+        error = input("Ingrese el Bit con Error: ")
+        if len(error) > 4:
+            print("error Mayor a 4 Bits")
+            print("Por favor, ingrese nuevamente.")
+        elif all(caracter == "0" or caracter == "1" for caracter in error):
+            return error
+        else:
+            print("Palabra no válida.")
+            print("Por favor, ingrese nuevamente.")
+
+
+error = obtener_error_valida()
+
+lista_error = list(error)
+
+# Paridad 1
+
+if int(lista_error[0]) == 1:
+    sumap1e += 1
+    sumap2e += 1
+else:
+    sumap1e = sumap1e
+    sumap2e = sumap2e
+
+if int(lista_error[1]) == 1:
+    sumap1e += 1
+    sumap3e += 1
+else:
+    sumap1e = sumap1e
+    sumap3e = sumap3e
+
+if int(lista_error[3]) == 1:
+    sumap1e += 1
+    sumap2e += 1
+    sumap3e += 1
+else:
+    sumap1e = sumap1e
+    sumap2e = sumap2e
+    sumap3e = sumap3e
+
+
+if sumap1e % 2:
+    v_paridad1e = 1
+else:
+    v_paridad1e = 0
+
+# Parida 2
+# lista0,lista2,lista3
+if int(lista_error[2]) == 1:
+    sumap2e += 1
+    sumap3e += 1
+else:
+    sumap2e = sumap2e
+    sumap3e = sumap3e
+
+if sumap2e % 2:
+    v_paridad2e = 1
+else:
+    v_paridad2e = 0
+
+# Paridad 3
+# lista1,lista2,lista3
+if sumap3e % 2:
+    v_paridad3e = 1
+else:
+    v_paridad3e = 0
+
+
+lista_info_error = [
+    str(v_paridad1e),
+    str(v_paridad2e),
+    lista_error[0],
+    str(v_paridad3e),
+    lista_error[1],
+    lista_error[2],
+    lista_error[3],
+]
+print(f"Codigo de Hamming: {''.join(lista_info)}")
+print(f"Codigo de Hamming Error: {''.join(lista_info_error)}")
+
+
+# Compuerta XOR
+
+# bit mas significativo paridad 3
+
+xora = bool(v_paridad3) ^ bool(v_paridad3e)
+xorb = bool(v_paridad2) ^ bool(v_paridad2e)
+xorc = bool(v_paridad1) ^ bool(v_paridad1e)
+
+if xora == True:
+    bita = 1
+else:
+    bita = 0
+
+if xorb == True:
+    bitb = 1
+else:
+    bitb = 0
+
+if xorc == True:
+    bitc = 1
+else:
+    bitc = 0
+
+
+biterror = [bita, bitb, bitc]
+num = "".join(map(str, biterror))
+if num == "011":
+    num = "100"
+else:
+    num = num
+for k in numeros_binarios:
+    if k == num:
+        break
+    else:
+        bitsuma += 1
+if bitsuma == 3:
+    bitsuma += 1
+else:
+    bitsuma = bitsuma
+
+if bitsuma == 0:
+    print("Su codigo no tiene Errores")
+else:
+    print(f"Su Error Esta en El Bit: {num}")
+    print(f"La Posicion del Error esta en el Bit: {bitsuma}")
+if bitsuma == 4:
+    lista_error[0] = "1" if lista_error[0] == "0" else "0"
+elif bitsuma == 5:
+    lista_error[1] = "1" if lista_error[1] == "0" else "0"
+elif bitsuma == 6:
+    lista_error[2] = "1" if lista_error[2] == "0" else "0"
+elif bitsuma == 7:
+    lista_error[3] = "1" if lista_error[3] == "0" else "0"
+
+print(f"Su Codigo correcto es: {''.join(lista_error)} ")
